97faf4b37dbad8d0b5e476dbdc4ea4b29f9411e0
JUan Andrade <jfandradea@gmail.com>
2021-08-18 16:56:10 -0500

    set up password reset method on auth config

--- keystone.ts
+++ keystone.ts
@@ -26,6 +26,11 @@ const { withAuth } = createAuth({
     fields: ['name', 'email', 'password'],
     /* todo: add an initial role here */
   },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    },
+  },
 });
 
 export default withAuth(
