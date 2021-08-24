b4109ed75d855998616ea88668ca4b01a13d4264
JUan Andrade <jfandradea@gmail.com>
2021-08-23 21:02:50 -0500

    custom mutation, extending resolvers in keystone

---
+++ mutations/addToCart.ts
@@ -0,0 +1,47 @@
+/* eslint-disable */
+import { KeystoneContext } from '@keystone-next/types';
+import { CartItemCreateInput } from '../.keystone/schema-types';
+import { Session } from '../types';
+
+export default async function addToCart(
+  root: any,
+  { productId }: { productId: string },
+  context: KeystoneContext
+): Promise<CartItemCreateInput> {
+  // 1. Query the current user and see if they're signed in
+  const session = context.session as Session;
+  console.log('us', session.itemId);
+  console.log('pr', productId);
+  if (!session.itemId) {
+    throw new Error('You must be logged in to do this');
+  }
+  // 2. Query the current user's cart
+  const allCartItems = await context.lists.CartItem.findMany({
+    where: { user: { id: session.itemId }, product: { id: productId } },
+    resolveFields: 'id,quantity',
+  });
+
+  console.log('ci', allCartItems);
+  // 3. See if the current item is in their cart
+  const [existingCartItem] = allCartItems; // destructure the first item
+  if (existingCartItem) {
+    console.log(existingCartItem);
+    console.log(
+      `There are already ${existingCartItem.quantity}, increment by 1!`
+    );
+    // 4. if it's, increment by 1
+    return await context.lists.CartItem.updateOne({
+      id: existingCartItem.id,
+      data: { quantity: existingCartItem.quantity + 1 },
+    });
+  }
+
+  // 4. if not, create a new cart item
+  return await context.lists.CartItem.createOne({
+    data: {
+      product: { connect: { id: productId } },
+      user: { connect: { id: session.itemId } },
+    },
+    resolveFields: false,
+  });
+}
