d6ad5cd6b6103461c2fc77381e242b627bd5ac63
JUan Andrade <jfandradea@gmail.com>
2021-10-15 20:27:30 -0500

    trying to deploy frontend local

--- keystone.ts
+++ keystone.ts
@@ -45,7 +45,7 @@ export default withAuth(
   config({
     server: {
       cors: {
-        origin: [process.env.FRONTEND_URL],
+        origin: [process.env.FRONTEND_URL, 'http://localhost:7777'],
         credentials: true,
       },
     },
